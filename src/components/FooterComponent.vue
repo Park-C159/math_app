ae1a2c7c68b7a2616cfe7a96d5b11555d8e57dcc
lkl <onebuaaer@gmail.com>
2024-11-28 20:43:49 +0800

    修改了样式，将图片与copyright放在了同一行

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,24 +1,26 @@
 <script setup lang="ts">
-  import {useRoute} from "vue-router";
-  import {computed} from "vue";
+import {useRoute} from "vue-router";
+import {computed} from "vue";
 
-  const route = useRoute();
+const route = useRoute();
 
-  // 判断是否是根路径
-  const isRootPath = computed(() => route.path !== '/');
+// 判断是否是根路径
+const isRootPath = computed(() => route.path !== '/');
 </script>
 
 <template>
-<footer class="footer">
-  <div class="bot-footer" :style="{ paddingTop: isRootPath ? '120px' : '20px' }">
-    <img src="@/assets/images/mathword.svg" width="150"><br /> Copyright © [Tianhao Yuan and Yongjun Wang] - 2024
-  </div>
-</footer>
+  <footer class="footer">
+    <div class="bot-footer" :style="{ paddingTop: isRootPath ? '120px' : '20px' }">
+      <div class="footer-content">
+        <img src="@/assets/images/mathword.svg" width="150">
+        <span class="copyright">Copyright © [Tianhao Yuan and Yongjun Wang] - 2024</span>
+      </div>
+    </div>
+  </footer>
 </template>
 
 <style scoped>
 .bot-footer {
-
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -30,4 +32,14 @@
   padding-bottom: 20px;
 }
 
+.footer-content {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+}
+
+.copyright {
+  font-size: 0.9em;
+  color: #666;
+}
 </style>
